095580fdc73efd3ac1087d1f76fa8e3441ec9a65
yorurv <109752341+yorurv@users.noreply.github.com>
2022-07-21 23:12:35 +0700

    Update Java_1_1_segitiga_a.java

--- Java/Java_1_1_segitiga_a.java
+++ Java/Java_1_1_segitiga_a.java
@@ -1,31 +1,31 @@
 /*
-	*        |\     
-	**       | \ 
-	***      |  \
-	****     |   \
-	*****    |____\  
+    *        |\     
+    **       | \ 
+    ***      |  \
+    ****     |   \
+    *****    |____\  
 */
 
 import java.util.Scanner;
 
 class Java_1_1_segitiga_a {
-	public static void main(String args[]) {
-		Scanner scn = new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
 
-		System.out.print("Masukkan Tinggi Segitiga : ");
-		int tinggi = scn.nextInt();
+        System.out.print("Masukkan Tinggi Segitiga : ");
+        int tinggi = scn.nextInt();
 
-		for(int i=0; i < tinggi; i++) {
-			System.out.print("|");
-			for(int j=0; j < i; j++) {
-				if(i == tinggi-1) {
-					System.out.print("_");
-				}
-				else {
-					System.out.print(" ");
-				}
-			}
-			System.out.println("\\");
-		}
-	}
+        for(int i=0; i < tinggi; i++) {
+            System.out.print("|");
+            for(int j=0; j < i; j++) {
+                if(i == tinggi-1) {
+                    System.out.print("_");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("\\");
+        }
+    }
 }
