d55bfbd7b3e860751ef106ca5217b6e8931d490e
yorurv <109752341+yorurv@users.noreply.github.com>
2022-07-21 23:07:38 +0700

    Create py_1_1_bilangan_ganjil_genap.py

---
+++ Python/py_1_1_bilangan_ganjil_genap.py
@@ -0,0 +1,12 @@
+"""
+Masukkan Bilangan : 7
+7 Adalah Bilangan Ganjil
+"""
+
+bilangan = input("Masukkan Bilangan : ")
+bilangan = int(bilangan)
+
+if bilangan % 2 == 0:
+	print(f"{bilangan} adalah Bilangan Genap")
+else:
+	print(f"{bilangan} adalah Bilangan Ganjil")
