d47ffb178725ac804ddde281df07b8a4807cd275
yorurv <109752341+yorurv@users.noreply.github.com>
2022-07-21 23:06:40 +0700

    Create python_1_1_faktorial.py

---
+++ Python/python_1_1_faktorial.py
@@ -0,0 +1,22 @@
+"""
+Masukkan Bilangan : 5
+5! = 5 * 4 * 3 * 2 * 1 = 120
+"""
+
+bilangan = input("Masukkan Bilangan : ")
+bilangan = int(bilangan)
+hasil = 1
+
+f_output = f"{bilangan}! = "
+
+while bilangan > 0:
+	f_output += f"{bilangan}"
+	hasil *= bilangan
+	bilangan -= 1
+
+	if bilangan < 1:
+		f_output += f" = {hasil}"
+	else:
+		f_output += " * "
+
+print(f_output)
