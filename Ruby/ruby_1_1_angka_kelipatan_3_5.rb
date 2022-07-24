74b1146ffb3d46ba5814e9f9893761e739acdd96
yorurv <109752341+yorurv@users.noreply.github.com>
2022-07-24 21:37:14 +0700

    Update ruby_1_1_angka_kelipatan_3_5.rb

--- Ruby/ruby_1_1_angka_kelipatan_3_5.rb
+++ Ruby/ruby_1_1_angka_kelipatan_3_5.rb
@@ -1,12 +1,12 @@
 =begin
-	Tugas Coding :
-	-> Contoh Output Program
+    Tugas Coding :
+    -> Contoh Output Program
 
-	Input batasan angka : 20
-	=====================================
-	Angka-angka yang merupakan kelipatan 3 atau 5 kurang dari 20 adalah : 3 5 6 9 10 12 15 18
-	Jumlah Angka Genap : 4 angka
-	Jumlah Angka Ganjil : 4 angka
+    Input batasan angka : 20
+    =====================================
+    Angka-angka yang merupakan kelipatan 3 atau 5 kurang dari 20 adalah : 3 5 6 9 10 12 15 18
+    Jumlah Angka Genap : 4 angka
+    Jumlah Angka Ganjil : 4 angka
 
 =end
 
@@ -21,16 +21,16 @@ jumlah_ganjil = 0
 
 for n in (1..(angka-1))
     # mencari angka kelipatan 3 / 5
-	if (n % 3 == 0) or (n % 5 == 0)
-		print "#{n} "
+    if (n % 3 == 0) or (n % 5 == 0)
+        print "#{n} "
 
         # mencari angka ganjil / genap
-		if n % 2 == 0
-			jumlah_genap += 1
-		else
-			jumlah_ganjil += 1
-		end
-	end
+        if n % 2 == 0
+            jumlah_genap += 1
+        else
+            jumlah_ganjil += 1
+        end
+    end
 end
 
 puts ""
