adc867e89a817b761a0e026ea9a601c356e55a15
Yoru Rv <109752341+yorurv@users.noreply.github.com>
2022-07-31 16:39:26 +0700

    Update pas_1_1_persegi_panjang.pas

--- PASCA/pas_1_1_persegi_panjang.pas
+++ PASCA/pas_1_1_persegi_panjang.pas
@@ -1,1 +1,29 @@
+(*
+    Program Persegi Panjang
 
+    Rumus Persegi Panjang
+        Keliling = 2 x ( panjang + lebar )
+        Luas = panjang x lebar
+*)
+
+program persegipanjang;
+
+var
+    panjang, lebar, keliling, luas : integer;
+
+begin
+    writeln('Program Persegi Panjang');
+    writeln('===========================');
+
+    write('Panjang : ');
+    read(panjang);
+
+    write('Lebar   : ');
+    read(lebar);
+
+    keliling := 2 * ( panjang + lebar );
+    writeln('Keliling = ', keliling);
+
+    luas := panjang * lebar;
+    writeln('Luas     = ', luas);
+end.
